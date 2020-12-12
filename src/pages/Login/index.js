ed7c401b177a6412dd45cf670c676c1b86303c25
breitenbach <cesar.breitenbach@gmail.com>
2020-12-12 07:24:46 -0300

    teste de CI

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -36,7 +36,7 @@ function Login() {
    }
    return (
       <Conteiner>
-      <Title>Login</Title>
+      <Title>Login - CI TESTE</Title>
       <LoginArea>
          {error &&
             <ErrorMessage>{error}</ErrorMessage>
